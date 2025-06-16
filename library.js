199304b17d7121ceaed7b5fae72ae2518149033b
Daik <fruit@kd6.so-net.ne.jp>
2025-06-17 02:26:23 +0900

    Add rating feature

--- library.js
+++ library.js
@@ -15,66 +15,108 @@ function addBookToLibrary(...rest) {
   myLibrary.push(...rest);
 }
 
-const doAndroidsDreamOfElectricSheep = new Book('Do Androids Dream of Electric Sheep?', 'Philip K. Dick', '210', 'not read yet');
-const nineteenEightyFour = new Book('Nineteen Eighty-Four', 'George Orwell', '328', 'not read yet');
-const theClient = new Book('The Client', 'John Grisham', '422', 'not read yet')
-const charlieAndTheChocolateFactory = new Book('Charlie and the Chocolate Factory', 'Roald Dahl', '155', 'not read yet');
-const myFathersDragon = new Book('My Father\'s Dragon', 'Ruth Stiles Gannett', '98', 'not read yet');
-const alicesAdventuresInWonderland = new Book('Alice\'s Adventures in Wonderland', 'Lewis Carroll', '172', 'not read yet');
+const doAndroidsDreamOfElectricSheep = new Book('Do Androids Dream of Electric Sheep?', 'Philip K. Dick', '210', 'five-stars');
+const nineteenEightyFour = new Book('Nineteen Eighty-Four', 'George Orwell', '328', 'five-stars');
+const theClient = new Book('The Client', 'John Grisham', '422', 'four-stars')
+const charlieAndTheChocolateFactory = new Book('Charlie and the Chocolate Factory', 'Roald Dahl', '155', 'four-stars');
+const myFathersDragon = new Book('My Father\'s Dragon', 'Ruth Stiles Gannett', '98', 'not-read-yet');
+const alicesAdventuresInWonderland = new Book('Alice\'s Adventures in Wonderland', 'Lewis Carroll', '172', 'not-read-yet');
 
 addBookToLibrary(doAndroidsDreamOfElectricSheep, nineteenEightyFour, theClient, charlieAndTheChocolateFactory, myFathersDragon, alicesAdventuresInWonderland);
 
 const bookshelf = document.querySelector('.bookshelf');
+const newBtn = document.querySelector('.new-btn');
+const dialogAdd = document.querySelector('.dialog-add');
+const cancelBtn = document.querySelectorAll('.cancel-btn');
+const addBtn = document.querySelector('.add-btn');
+const confirmBtn = document.querySelector('.confirm-btn');
+const inputTitle = document.getElementById('title');
+const inputAuthor = document.getElementById('author');
+const inputPages = document.getElementById('pages');
+const ratingNewBook = document.getElementById('rating-new-book');
+const dialogRating = document.querySelector('.dialog-rating');
+const ratingOldBook = document.getElementById('rating-old-book');
+let itemId;
 
 renderLibrary();
 
 function renderLibrary() {
   bookshelf.innerHTML = '';
+  
   myLibrary.forEach((book) => {
-  const item = document.createElement('li');
-  const title = document.createElement('h4');
-  const author = document.createElement('p');
-  const pages = document.createElement('p');
-  const btnContainer = document.createElement('p');
-  const ratingBtn = document.createElement('button');
-  const deleteBtn = document.createElement('button');
-  title.textContent = book.title;
-  author.textContent = book.author;
-  pages.textContent = book.pages + ' pages';
-  btnContainer.classList = 'btn-container';
-  ratingBtn.classList = 'rating-btn';
-  ratingBtn.textContent = book.rating;
-  deleteBtn.classList = 'delete-btn';
-  deleteBtn.innerHTML = '&#10005;';
-  item.appendChild(title);
-  item.appendChild(author);
-  item.appendChild(pages);
-  btnContainer.appendChild(ratingBtn);
-  btnContainer.appendChild(deleteBtn);
-  item.appendChild(btnContainer);
-  bookshelf.appendChild(item);
-
-  ratingBtn.addEventListener('click', () => {
+    const item = document.createElement('li');
+    const title = document.createElement('h4');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const btnContainer = document.createElement('p');
+    const ratingBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
     
-  });
+    item.setAttribute('data-id', `${book.id}`);
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages + ' pages';
+    btnContainer.classList = 'btn-container';
+    ratingBtn.classList = 'rating-btn';
+    
+    if (book.rating === 'five-stars') {
+      ratingBtn.innerHTML = '&#9733;&#9733;&#9733;&#9733;&#9733;';
+    } else if (book.rating === 'four-stars') {
+      ratingBtn.innerHTML = '&#9733;&#9733;&#9733;&#9733;'
+    } else if (book.rating === 'three-stars') {
+      ratingBtn.innerHTML = '&#9733;&#9733;&#9733;'
+    } else if (book.rating === 'two-stars') {
+      ratingBtn.innerHTML = '&#9733;&#9733;'
+    } else if (book.rating === 'one-star') {
+      ratingBtn.innerHTML = '&#9733;'
+    } else {
+      ratingBtn.textContent = book.rating;
+    }
+    
 
-  deleteBtn.addEventListener('click', (e) => {
-    bookshelf.removeChild(e.target.parentNode.parentNode);
+    deleteBtn.classList = 'delete-btn';
+    deleteBtn.innerHTML = '&#10005;';
+    item.appendChild(title);
+    item.appendChild(author);
+    item.appendChild(pages);
+    btnContainer.appendChild(ratingBtn);
+    btnContainer.appendChild(deleteBtn);
+    item.appendChild(btnContainer);
+    bookshelf.appendChild(item);
+
+    ratingBtn.addEventListener('click', (e) => {
+      itemId = e.target.parentNode.parentNode.dataset.id;
+      dialogRating.showModal();
+    });
+
+    cancelBtn.forEach((btn) => {
+      btn.addEventListener('click', () => {
+        dialogRating.close();
+      });
+    });
+
+    deleteBtn.addEventListener('click', (e) => {
+      bookshelf.removeChild(e.target.parentNode.parentNode);
+    });
   });
-});
 }
 
-const newBtn = document.querySelector('.new-btn');
-const dialog = document.querySelector('.dialog');
-const cancelBtn = document.querySelector('.cancel-btn');
-const addBtn = document.querySelector('.add-btn');
-const inputTitle = document.getElementById('title');
-const inputAuthor = document.getElementById('author');
-const inputPages = document.getElementById('pages');
-const selectRating = document.getElementById('rating');
+
+confirmBtn.addEventListener('click', () => {
+  let rating = ratingOldBook.value;
+
+  myLibrary.forEach((book) => {
+    if (book.id === itemId) {
+      book.rating = rating;
+    }
+  });
+  renderLibrary();
+  dialogRating.close();
+});
+
 
 newBtn.addEventListener('click', (e) => {
-  dialog.showModal();
+  dialogAdd.showModal();
   document.addEventListener('keydown', () => {
     if (e.key === 'Enter') {
       
@@ -82,18 +124,20 @@ newBtn.addEventListener('click', (e) => {
   });
 });
 
-cancelBtn.addEventListener('click', () => {
-  dialog.close();
+cancelBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    dialogAdd.close();
+  });
 });
 
 addBtn.addEventListener('click', () => {
   let title = inputTitle.value;
   let author = inputAuthor.value;
   let pages = inputPages.value;
-  let rating = selectRating.value;
+  let rating = ratingNewBook.value;
 
   const newBook = new Book(title, author, pages, rating);
   addBookToLibrary(newBook);
   renderLibrary();
-  dialog.close();
+  dialogAdd.close();
 });
